af0132e2d95da68fabb990651b79c862f9fbfb67
TheAwiteb <awiteb@hotmail.com>
2023-03-04 21:08:30 +0300

    Run the bot

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
+mod bot;
 mod cli;
 mod errors;
+mod utils;
 
-fn try_main() -> errors::Result<()> {
+async fn try_main() -> errors::Result<()> {
     // Skip the first argument, which is the program name.
     let cli = cli::Cli::parse(std::env::args().skip(1).collect())?;
     if cli.help {
         println!("{}", cli::help_message());
     } else if cli.version {
         println!("{}", cli::version_message());
+    } else {
+        bot::run(cli).await?;
     }
     Ok(())
 }
 
-fn main() {
-    if let Err(err) = try_main() {
+#[tokio::main]
+async fn main() {
+    if let Err(err) = try_main().await {
         eprintln!("Error: {}", err);
         std::process::exit(1);
     }
