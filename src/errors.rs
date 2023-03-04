ef55f02fdfd3a85bff4648b2b7d958613e399f63
TheAwiteb <awiteb@hotmail.com>
2023-03-04 21:07:29 +0300

    Add more error

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone, thiserror::Error)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("Unknown argument `{0}`")]
     UnknownArgument(String),
@@ -8,6 +8,26 @@ pub enum Error {
     UncompletedArgument(String),
     #[error("In argument `{0}`: {1}")]
     Parsing(String, String),
+    #[error("There is no publish date in items of the feed `{0}`")]
+    NoPublishDate(url::Url),
+    #[error("The publish date of the feed `{0}` is invalid")]
+    InvalidPublishDate(url::Url),
+    #[error("There is no title in items of the feed `{0}`")]
+    NoTitle(url::Url),
+    #[error("There is no link in items of the feed `{0}`")]
+    NoLink(url::Url),
+    #[error("There is no description in items of the feed `{0}`")]
+    NoDescription(url::Url),
+    #[error("Invalid feed URL: {0}")]
+    InvalidUrl(#[from] url::ParseError),
+    #[error("Request error: {0}")]
+    RequestError(#[from] reqwest::Error),
+    #[error("RSS error: {0}")]
+    RssError(#[from] rss::Error),
+    #[error("IO error: {0}")]
+    IoError(#[from] std::io::Error),
+    #[error("Megalodon error: {0}")]
+    MegalodonError(#[from] megalodon::error::Error),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
